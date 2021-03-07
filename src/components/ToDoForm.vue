73dc11efd97df01c0b0e390b6e812422ff979a4a
alireza <alireza.lm69@gmail.com>
2021-03-07 19:52:22 -0300

    first version

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="to-do-form">
+
+    <form @submit.prevent="onSubmit()">
+      <h2 class="label-wrapper">
+        <label for="new-todo" class="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input class="input__lg" v-model.trim="label" id="new-todo" type="text" name="new-todo" autocomplete="off">
+      <button class="btn btn__primary btn__lg" type="submit" > Add</button>
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoForm",
+  data() {
+    return {
+      label: ""
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit('todo-added', this.label)
+      this.label = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
